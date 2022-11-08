54a88dbd5029f5fa26b2d10432284a600704e92a
Maciej Retowski <retowski@protonmail.com>
2022-11-08 12:39:21 +0100

    Create the structure and blueprint of the flow of the app

---
+++ src/com/findwise/Main.java
@@ -0,0 +1,7 @@
+package com.findwise;
+
+public class Main {
+    public static void main(String[] args) {
+        // create app object that will control the flow of the app
+    }
+}
---
+++ src/com/findwise/SearchEngineApp.java
@@ -0,0 +1,8 @@
+package com.findwise;
+
+public class SearchEngineApp {
+    // get the documents from the user
+    // index them into the database
+    // get the search keyword from the user
+    // print the documents ids in correct order
+}
